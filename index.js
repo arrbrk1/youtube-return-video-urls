a1b2d4ad8b319515705ee76bc04980ee2f26ac04
arrbrk1 <acorelado@gmail.com>
2021-08-05 00:43:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+const arr = []
+const links = document.querySelectorAll("a#video-title")
+for(let i = 0; i < links.length; i++){
+    arr.push(`youtube.com${links[i].getAttribute("href")}`)
+}
+console.log(arr)
